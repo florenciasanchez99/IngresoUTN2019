0d3d7b1579292cd35ddf1fefe186e604a764f8ea
florenciasanchez99 <47385930+florenciasanchez99@users.noreply.github.com>
2019-02-13 12:23:53 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -7,10 +7,33 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
+    var largo;
+    var ancho;
+    var perimetro;
+    var alambre;
+
+
+     largo= parseFloat(document.getElementById("Largo").value);
+     ancho= parseFloat(document.getElementById("Ancho").value);
+
+    perimetro= (largo + ancho) *2;
+    alambre= perimetro*3;
+    alert("La cantidad de alambre a comprar es " + alambre);
+
+
+
 }
 function Circulo () 
 {
-	
+    
+var radio;
+var diametro;
+var alambre;
+
+radio=parseFloat(document.getElementById("Radio").value);
+diametro= radio*2*Math.PI;
+alambre=diametro*3;
+alert("La cantidad de alambre a comprar es " + alambre);
 }
 function Materiales () 
 {
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,59 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    
+    var precio1;
+    var precio2;
+    var precio3;
+    var resultado;
+
+precio1=parseInt(document.getElementById("PrecioUno").value);
+precio2= parseInt(document.getElementById("PrecioDos").value);
+precio3= parseInt(document.getElementById("PrecioTres").value);
+
+resultado = precio1 + precio2 + precio3;
+alert("La suma es "+ resultado);
+
 }
 function Promedio () 
 {
-	
+    var precio1;
+    var precio2;
+    var precio3;
+    var resultado;
+ 
+ precio1=parseInt(document.getElementById("PrecioUno").value);
+precio2= parseInt(document.getElementById("PrecioDos").value);
+precio3= parseInt(document.getElementById("PrecioTres").value);
+
+resultado = (precio1 + precio2 + precio3) / 3;
+alert("El importe de los precios es "+ resultado);
+ 
+
+
 }
 function PrecioFinal () 
 {
-	
+    
+    var precio1;
+    var precio2;
+    var precio3;
+    var suma;
+    var iva;
+    var resultadoConIva;
+    
+    precio1=parseFloat(document.getElementById("PrecioUno").value);
+    precio2= parseFloat(document.getElementById("PrecioDos").value);
+    precio3= parseFloat(document.getElementById("PrecioTres").value);
+    
+    suma= precio1 + precio2 + precio3;
+    iva= suma * 21/ 100;
+    resultadoConIva= suma + iva;
+
+    alert("El precio con IVA incluido es" + resultadoConIva);
+
 }
+
+
+
+
