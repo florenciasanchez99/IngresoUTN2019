8bcf4c1e7f25cef08f6bcd1fbee6a28b0cf576ea
florenciasanchez99 <47385930+florenciasanchez99@users.noreply.github.com>
2019-02-14 12:22:09 -0300

    Add files via upload

---
+++ jsInstruccionIF (3).js
@@ -0,0 +1,17 @@
+function Mostrar()
+{
+    var edad;
+
+    edad= parseInt(document.getElementById("edad").value);
+    
+    if( edad >=18){
+    
+        alert("Es mayor de edad");
+    }
+
+        else {
+            alert("Es menor de edad");
+        }
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,15 @@
+function Mostrar()
+{
+//tomo la edad  
+
+    var edad;
+    var estadoCivil;
+
+    edad=parseInt(document.getElementById("edad").value);
+estadoCivil=parseInt(document.getElementById("estadoCivil").value);
+if (estado== "Soltero"&&!(edad <18)){
+    alert("Es soltero y no es menor");
+}
+
+
+}//FIN DE LA FUNCIÓN
